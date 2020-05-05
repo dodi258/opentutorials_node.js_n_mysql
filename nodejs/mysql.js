ef7ac21fed985fb7e5237f828eb98c802d743523
Dodie Joo <dodi258@naver.com>
2020-05-05 16:54:56 +0900

    mysql 수정
    url의 query를 받아서 그것의 정보로 db를 접근하여 update, create을 해본다.
    
    - insert와 동시에 insert한 row의 id를 받아 홈페이지 주소를 redirect하는 코드도 있음.

---
+++ nodejs/mysql.js
@@ -0,0 +1,18 @@
+let mysql = require("mysql");
+let connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "wnwn102930!",
+  database: "opentutorials",
+});
+
+connection.connect();
+
+connection.query("SELECT * FROM topic", function (error, results, fields) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+});
+
+connection.end();
