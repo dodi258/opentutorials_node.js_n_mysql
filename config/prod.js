54ac764f27c9203825ed6a6cf904c17ef0aadd81
Dodie Joo <dodi258@naver.com>
2020-05-05 18:42:24 +0900

    Create prod.js
    production 모드일 경우에 사용하는 mysql 변수들을 prod.js 에 저장한다.

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// 사용하는 배포 서버에 직접 정의해야함. 
+module.exports = {
+    mysqlHOST: process.env.mysqlHOST,
+    mysqlUSER: process.env.mysqlUSER, 
+    mysqlPASSWORD: process.env.mysqlPASSWORD, 
+    mysqlDATABASE: process.env.mysqlDATABASE
+}
